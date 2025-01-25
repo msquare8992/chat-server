02d4e4358509400458c262d21cec3744ffc13f84
msquare8992 <msquare8992@gmail.com>
2025-01-25 16:29:59 +0530

    url updated

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = socketIo(server, {
     cors: {
-        origin: 'http://localhost:4200',
+        origin: 'https://msquare8992.github.io/chat',
         methods: ['GET', 'POST'],
         allowedHeaders: ['content-type'],
         credentials: true
@@ -16,7 +16,7 @@ const io = socketIo(server, {
 });
 
 app.use(cors({
-    origin: 'http://localhost:4200',
+    origin: 'https://msquare8992.github.io/chat',
     methods: ['GET', 'POST'],
     allowedHeaders: ['content-type'],
     credentials: true
