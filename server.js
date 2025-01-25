98c2a0e564f3464e4e9e926b115964695ce93bdf
msquare8992 <msquare8992@gmail.com>
2025-01-25 15:45:48 +0530

    Initial commit for backend

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+
+const io = socketIo(server, {
+    cors: {
+        origin: 'http://localhost:4200',
+        methods: ['GET', 'POST'],
+        allowedHeaders: ['content-type'],
+        credentials: true
+    }
+});
+
+app.use(cors({
+    origin: 'http://localhost:4200',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['content-type'],
+    credentials: true
+}));
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Chat App Server');
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
