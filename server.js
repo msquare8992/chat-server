a5f0857afaccf6827c1ea68effaa598637648fee
msquare8992 <msquare8992@gmail.com>
2025-01-28 19:10:34 +0530

    ui changes

--- server.js
+++ server.js
@@ -6,12 +6,10 @@ const cors = require('cors');
 const app = express();
 const server = http.createServer(app);
 
-let userList = {};
-
 const io = socketIo(server, {
     cors: {
-        origin: 'https://msquare8992.github.io',
-        // origin: 'http://localhost:4200',
+        // origin: 'https://msquare8992.github.io',
+        origin: 'http://localhost:4200',
         methods: ['GET', 'POST'],
         allowedHeaders: ['content-type'],
         credentials: true
@@ -19,8 +17,8 @@ const io = socketIo(server, {
 });
 
 app.use(cors({
-    origin: 'https://msquare8992.github.io',
-    // origin: 'http://localhost:4200',
+    // origin: 'https://msquare8992.github.io',
+    origin: 'http://localhost:4200',
     methods: ['GET', 'POST'],
     allowedHeaders: ['content-type'],
     credentials: true
@@ -30,21 +28,25 @@ app.get('/', (req, res) => {
   res.send('Welcome to Chat App Server');
 });
 
+let userList = {};
+
 io.on('connection', (socket) => {
-    socket.on('register', (sender) => {
-        userList[sender] = socket.id;
+    socket.on('register', (username) => {
+        userList[username] = socket.id;
+        console.log(`User registered: ${username} with scoket id: ${socket.id}`);
+        for(let user in userList) {
+            io.emit('userStatus', {username: user, status: true});
+        }
     })
 
     socket.on('sendMessage', (data) => {
         const { sender, receiver, message } = data;
         if(userList[receiver]) {
-            io.to(userList[sender]).emit('userStatus', {status: true});
             io.to(userList[sender]).emit('receiveMessage', {sender, message});
             io.to(userList[receiver]).emit('receiveMessage', {sender, message});
         }
         else {
-            console.log(`User ${ receiver } is not online`);
-            io.to(userList[sender]).emit('userStatus', {status: false});
+            io.to(userList[sender]).emit('receiveMessage', {sender, message});
         }
     });
 
@@ -52,7 +54,8 @@ io.on('connection', (socket) => {
         for(let username in userList) {
             if(userList[username] === socket.id) {
                 delete userList[username];
-                console.log(`${username} disconnected`);
+                io.emit('userStatus', {username, status: false});
+                console.log(`${username} disconnected and removed from userList`);
                 break;
             }
         }
