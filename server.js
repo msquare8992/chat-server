3a885da59a90321e698bd87c97900e293f70f124
msquare8992 <msquare8992@gmail.com>
2025-01-28 19:11:08 +0530

    url changes

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const server = http.createServer(app);
 
 const io = socketIo(server, {
     cors: {
-        // origin: 'https://msquare8992.github.io',
-        origin: 'http://localhost:4200',
+        origin: 'https://msquare8992.github.io',
+        // origin: 'http://localhost:4200',
         methods: ['GET', 'POST'],
         allowedHeaders: ['content-type'],
         credentials: true
@@ -17,8 +17,8 @@ const io = socketIo(server, {
 });
 
 app.use(cors({
-    // origin: 'https://msquare8992.github.io',
-    origin: 'http://localhost:4200',
+    origin: 'https://msquare8992.github.io',
+    // origin: 'http://localhost:4200',
     methods: ['GET', 'POST'],
     allowedHeaders: ['content-type'],
     credentials: true
